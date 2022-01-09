a007b21cc8af2c8b77abc5f7da8ce3ecb33de366
iverly <iverly.dev@gmail.com>
2022-01-09 16:33:01 +0100

    feat: add _document & add theme in _app

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,18 @@
+import Head from 'next/head';
+import ThemeProvider from '../theme';
+
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <Head>
+        <meta name="viewport" content="initial-scale=1, width=device-width" />
+      </Head>
+
+      <ThemeProvider>
+        <Component {...pageProps} />
+      </ThemeProvider>
+    </>
+  );
 }
 
 export default MyApp;
