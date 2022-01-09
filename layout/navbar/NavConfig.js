2c471e0e47443ba1cc339accfcbe2aaef73b4402
iverly <iverly.dev@gmail.com>
2022-01-09 19:46:34 +0100

    feat: add navbar items

--- layout/navbar/NavConfig.js
+++ layout/navbar/NavConfig.js
@@ -1,15 +1,63 @@
+import Iconify from '../../components/Iconify';
+
 const navConfig = [
   {
     subheader: 'general',
     items: [
       {
-        title: 'app',
+        title: 'tickets',
         path: '/',
+        icon: <Iconify icon="entypo:ticket" />,
       },
-      { title: 'e-commerce', path: '/ecommerce' },
-      { title: 'analytics', path: '/analytics' },
-      { title: 'banking', path: '/banking' },
-      { title: 'booking', path: '/booking' },
+      {
+        title: 'calendar',
+        path: '/calendar',
+        icon: <Iconify icon="bi:calendar-date-fill" />,
+      },
+      {
+        title: 'checkout',
+        path: '/checkout',
+        icon: <Iconify icon="ic:round-shopping-cart-checkout" />,
+      },
+    ],
+  },
+  {
+    subheader: 'account',
+    items: [
+      {
+        title: 'profile',
+        path: '/profile',
+        icon: <Iconify icon="ri:profile-fill" />,
+      },
+      {
+        title: 'settings',
+        path: '/settings',
+        icon: <Iconify icon="akar-icons:settings-vertical" />,
+      },
+      {
+        title: 'billings',
+        path: '/billings',
+        icon: <Iconify icon="bi:credit-card-fill" />,
+      },
+    ],
+  },
+  {
+    subheader: 'admin',
+    items: [
+      {
+        title: 'manage',
+        path: '/admin',
+        icon: <Iconify icon="whh:indexmanager" />,
+      },
+    ],
+  },
+  {
+    items: [
+      {
+        title: 'logout',
+        path: '/logout',
+        icon: <Iconify icon="clarity:logout-solid" />,
+      },
     ],
   },
 ];
