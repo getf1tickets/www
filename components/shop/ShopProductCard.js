728767ee84bfd27612497844a92ba0813bb5546f
iverly <iverly.dev@gmail.com>
2022-01-12 12:24:48 +0100

    feat: make image for ticket card clickable

--- components/shop/ShopProductCard.js
+++ components/shop/ShopProductCard.js
@@ -7,31 +7,35 @@ import Image from '../Image';
 
 export default function ShopProductCard({ product }) {
   const {
-    name, cover, status, priceSale, price,
+    id, name, cover, status, priceSale, price,
   } = product;
 
-  const linkTo = `ticket/${name}`;
+  const linkTo = `ticket/${id}`;
 
   return (
     <Card>
-      <Box sx={{ position: 'relative' }}>
-        {status && (
-          <Label
-            variant="filled"
-            color={(status === 'out of stock' && 'error') || 'info'}
-            sx={{
-              top: 16,
-              right: 16,
-              zIndex: 9,
-              position: 'absolute',
-              textTransform: 'uppercase',
-            }}
-          >
-            {status}
-          </Label>
-        )}
-        <Image alt={name} src={cover} ratio="6/4" />
-      </Box>
+      <NextLink href={linkTo} passHref>
+        <Link color="inherit">
+          <Box sx={{ position: 'relative' }}>
+            {status && (
+            <Label
+              variant="filled"
+              color={(status === 'out of stock' && 'error') || 'info'}
+              sx={{
+                top: 16,
+                right: 16,
+                zIndex: 9,
+                position: 'absolute',
+                textTransform: 'uppercase',
+              }}
+            >
+              {status}
+            </Label>
+            )}
+            <Image alt={name} src={cover} ratio="6/4" />
+          </Box>
+        </Link>
+      </NextLink>
 
       <Stack spacing={2} sx={{ p: 3 }}>
         <Stack direction="row" alignItems="center" justifyContent="space-between">
