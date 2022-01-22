bc421da6e8de4c58a75a51877275aa487e871cbd
iverly <iverly.dev@gmail.com>
2022-01-22 16:24:02 +0100

    feat: account settings data from context

--- components/account/AccountGeneral.js
+++ components/account/AccountGeneral.js
@@ -1,8 +1,9 @@
 import * as Yup from 'yup';
+import { useMemo, useEffect } from 'react';
 import { useForm } from 'react-hook-form';
 import { yupResolver } from '@hookform/resolvers/yup';
 import {
-  Box, Grid, Card, Stack, Typography,
+  Box, Card, Stack, Typography,
 } from '@mui/material';
 import { LoadingButton } from '@mui/lab';
 import useUser from '../../hooks/useUser';
@@ -11,25 +12,22 @@ import {
 } from '../form';
 
 export default function AccountGeneral() {
-  const user = useUser();
+  const { email, info } = useUser();
 
   const UpdateUserSchema = Yup.object().shape({
-    displayName: Yup.string().required('Name is required'),
+    name: Yup.string().required('Name is required'),
   });
 
-  const defaultValues = {
-    displayName: user?.displayName || '',
-    email: user?.email || '',
-    photoURL: user?.photoURL || '',
-    phoneNumber: user?.phoneNumber || '',
-    country: user?.country || '',
-    address: user?.address || '',
-    state: user?.state || '',
-    city: user?.city || '',
-    zipCode: user?.zipCode || '',
-    about: user?.about || '',
-    isPublic: user?.isPublic || '',
-  };
+  const defaultValues = useMemo(() => ({
+    name: info?.name || '',
+    email: email || '',
+    phoneNumber: info?.phoneNumber || '',
+    country: info?.country || '',
+    address: info?.address || '',
+    state: info?.state || '',
+    city: info?.city || '',
+    zipCode: info?.zipCode || '',
+  }), [email, info]);
 
   const methods = useForm({
     resolver: yupResolver(UpdateUserSchema),
@@ -38,8 +36,20 @@ export default function AccountGeneral() {
 
   const {
     handleSubmit,
+    setValue,
     formState: { isSubmitting },
   } = methods;
+
+  useEffect(() => {
+    setValue('name', info?.name || '');
+    setValue('email', email || '');
+    setValue('phoneNumber', info?.phoneNumber || '');
+    setValue('country', info?.country || '');
+    setValue('address', info?.address || '');
+    setValue('state', info?.state || '');
+    setValue('city', info?.city || '');
+    setValue('zipCode', info?.zipCode || '');
+  }, [setValue, info, email]);
 
   const onSubmit = async (data) => {
     console.log(data);
@@ -47,55 +57,45 @@ export default function AccountGeneral() {
 
   return (
     <FormProvider methods={methods} onSubmit={handleSubmit(onSubmit)}>
-      <Grid spacing={1}>
-        {/* <Grid item xs={12} md={4}>
-          <Card sx={{ py: 10, px: 3, textAlign: 'center' }}>
-            <Avatar color="default" />
-          </Card>
-        </Grid> */}
+      <Card sx={{ p: 3 }}>
+        <Stack sx={{ pb: 4 }} spacing={3} alignItems="flex-start">
+          <Typography variant="overline" sx={{ color: 'text.secondary' }}>
+            Account information
+          </Typography>
+        </Stack>
+        <Box
+          sx={{
+            display: 'grid',
+            rowGap: 3,
+            columnGap: 2,
+            gridTemplateColumns: { xs: 'repeat(1, 1fr)', sm: 'repeat(2, 1fr)' },
+          }}
+        >
+          <RHFTextField name="name" label="Name" />
+          <RHFTextField disabled name="email" label="Email Address" />
 
-        <Grid xs={12} md={12}>
-          <Card sx={{ p: 3 }}>
-            <Stack sx={{ pb: 4 }} spacing={3} alignItems="flex-start">
-              <Typography variant="overline" sx={{ color: 'text.secondary' }}>
-                Account information
-              </Typography>
-            </Stack>
-            <Box
-              sx={{
-                display: 'grid',
-                rowGap: 3,
-                columnGap: 2,
-                gridTemplateColumns: { xs: 'repeat(1, 1fr)', sm: 'repeat(2, 1fr)' },
-              }}
-            >
-              <RHFTextField name="displayName" label="Name" />
-              <RHFTextField disabled name="email" label="Email Address" />
+          <RHFTextField name="phoneNumber" label="Phone Number" />
+          <RHFTextField name="address" label="Address" />
 
-              <RHFTextField name="phoneNumber" label="Phone Number" />
-              <RHFTextField name="address" label="Address" />
+          <RHFSelect name="country" label="Country" placeholder="Country">
+            {/* <option value="" /> */}
+            <option value="fr">
+              France
+            </option>
+          </RHFSelect>
 
-              <RHFSelect name="country" label="Country" placeholder="Country">
-                {/* <option value="" /> */}
-                <option value="fr">
-                  France
-                </option>
-              </RHFSelect>
+          <RHFTextField name="state" label="State/Region" />
 
-              <RHFTextField name="state" label="State/Region" />
+          <RHFTextField name="city" label="City" />
+          <RHFTextField name="zipCode" label="Zip/Code" />
+        </Box>
 
-              <RHFTextField name="city" label="City" />
-              <RHFTextField name="zipCode" label="Zip/Code" />
-            </Box>
-
-            <Stack spacing={3} alignItems="flex-end" sx={{ mt: 3 }}>
-              <LoadingButton type="submit" variant="contained" loading={isSubmitting}>
-                Save Changes
-              </LoadingButton>
-            </Stack>
-          </Card>
-        </Grid>
-      </Grid>
+        <Stack spacing={3} alignItems="flex-end" sx={{ mt: 3 }}>
+          <LoadingButton type="submit" variant="contained" loading={isSubmitting}>
+            Save Changes
+          </LoadingButton>
+        </Stack>
+      </Card>
     </FormProvider>
   );
 }
