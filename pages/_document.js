d2aa2b273022ec5a46d601823f9a170927a1f205
iverly <iverly.dev@gmail.com>
2022-02-06 13:40:50 +0100

    feat: add f1 tickets logo

--- pages/_document.js
+++ pages/_document.js
@@ -11,9 +11,9 @@ export default class MyDocument extends Document {
         <Head>
           <meta charSet="utf-8" />
 
-          {/* <link rel="apple-touch-icon" sizes="180x180" href="/favicon/apple-touch-icon.png" />
-          <link rel="icon" type="image/png" sizes="32x32" href="/favicon/favicon-32x32.png" />
-          <link rel="icon" type="image/png" sizes="16x16" href="/favicon/favicon-16x16.png" /> */}
+          <link rel="apple-touch-icon" sizes="180x180" href="https://s3.getf1tickets.com/public/logo.ico" />
+          <link rel="icon" type="image/png" sizes="32x32" href="https://s3.getf1tickets.com/public/logo.ico" />
+          <link rel="icon" type="image/png" sizes="16x16" href="https://s3.getf1tickets.com/public/logo.ico" />
 
           <meta name="theme-color" content={palette.light.primary.main} />
           {/* <link rel="manifest" href="/manifest.json" /> */}
